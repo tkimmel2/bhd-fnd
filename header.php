8ced04afaf4b6b7ee92bcab03e7caa6177225400
Travis Kimmel <travis@brickhousedevelopment.com>
2012-11-05 12:45:29 -0700

    added upcoming events page

--- header.php
+++ header.php
@@ -30,12 +30,12 @@ function print_header($active = 'home') {
 
       <!-- main navigation -->
       <ul class="nav-bar">
-        <li<?php if($active == 'home') { ?> class="active"<?php } ?>><a href="/">Home</a></li>
+        <li<?php if($active == 'home') { ?> class="active"<?php } ?>><a href="index.php">Home</a></li>
         <li class="<?php if(stripos($active, 'events') !== FALSE) { ?>active <?php } ?>has-flyout">
           <a href="#">Events</a>
           <a href="#" class="flyout-toggle"><span> </span></a>
           <ul class="flyout">
-            <li<?php if($active == 'events1') { ?> class="active"<?php } ?>><a href="#">Sub Nav Item 1</a></li>
+            <li<?php if($active == 'events1') { ?> class="active"<?php } ?>><a href="upcoming_events.php">Upcoming Events</a></li>
             <li<?php if($active == 'events2') { ?> class="active"<?php } ?>><a href="#">Sub Nav Item 2</a></li>
             <li<?php if($active == 'events3') { ?> class="active"<?php } ?>><a href="#">Sub Nav 3</a></li>
             <li<?php if($active == 'events4') { ?> class="active"<?php } ?>><a href="#">Sub Nav 4</a></li>
---
+++ upcoming_events.php
@@ -0,0 +1,110 @@
+<?php
+include('header.php');
+// valid arguments: 
+//  1st Level:
+//   home (default)
+//   events
+//   news
+//   resources
+//  2nd Level:
+//   events*
+//   news*
+//   resources*
+print_header('upcoming events');
+?>
+
+	<div class="row">
+		<div class="eight columns">
+			<h3>Upcoming Events</h3>
+
+			<!-- Grid Example -->
+			<div class="row">
+				<div class="twelve columns">
+					<div>
+						<p>This paragraph is optional, and will be shown above the listing of News posts. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="twelve columns">
+          <h4>Event 2</h4>
+					<div class="panel">
+            <p>Nov 5, 2012</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="twelve columns">
+          <h4>Event 3</h4>
+					<div class="panel">
+            <p>Nov 9, 2012</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="twelve columns">
+          <h4>Event 4</h4>
+					<div class="panel">
+            <p>Nov 15, 2012</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="twelve columns">
+          <h4>Event 5</h4>
+					<div class="panel">
+            <p>December 31, 2012</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="twelve columns">
+          <h4>Event 6</h4>
+					<div class="panel">
+            <p>January 1, 2012</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					</div>
+				</div>
+			</div>
+
+      <ul class="pagination offset-by-three">
+        <li class="arrow unavailable"><a href="">&laquo;</a></li>
+        <li class="current"><a href="">1</a></li>
+        <li><a href="">2</a></li>
+        <li><a href="">3</a></li>
+        <li><a href="">4</a></li>
+        <li class="unavailable"><a href="">&hellip;</a></li>
+        <li><a href="">12</a></li>
+        <li><a href="">13</a></li>
+        <li class="arrow"><a href="">&raquo;</a></li>
+      </ul>
+
+		</div>
+
+		<div class="four columns">
+			<h4>Event Archive</h4>
+			<p>Find past articles in the archive below:</p>
+
+			<ul class="square">
+				<li><a href="#">November (1)</a></li>
+				<li><a href="#">October (4)</a></li>
+				<li><a href="#">September (5)</a></li>
+				<li><a href="#">August (3)</a></li>
+				<li><a href="#">July (3)</a></li>
+				<li><a href="#">June (5)</a></li>
+				<li><a href="#">May (3)</a></li>
+				<li><a href="#">April (1)</a></li>
+				<li><a href="#">March (3)</a></li>
+				<li><a href="#">February (5)</a></li>
+				<li><a href="#">January (7)</a></li>
+				<li><a href="#">December (1)</a></li>
+			</ul>
+		</div>
+	</div>
+
+<?php include('footer.php'); ?>
+
