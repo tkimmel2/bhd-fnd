f6c796c8c88b567059c6eba29094a71e1ec31b1d
Travis Kimmel <travis@brickhousedevelopment.com>
2012-11-18 12:57:08 -0700

    changed ordering on event page cause it looked derpy

--- upcoming_events.php
+++ upcoming_events.php
@@ -27,7 +27,7 @@ print_header('upcoming_events');
 			</div>
 			<div class="row">
 				<div class="twelve columns">
-          <h4>Event 2</h4>
+          <h4>Event 1</h4>
 					<div class="panel">
             <p>Nov 5, 2012</p>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -36,7 +36,7 @@ print_header('upcoming_events');
 			</div>
 			<div class="row">
 				<div class="twelve columns">
-          <h4>Event 3</h4>
+          <h4>Event 2</h4>
 					<div class="panel">
             <p>Nov 9, 2012</p>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -45,7 +45,7 @@ print_header('upcoming_events');
 			</div>
 			<div class="row">
 				<div class="twelve columns">
-          <h4>Event 4</h4>
+          <h4>Event 3</h4>
 					<div class="panel">
             <p>Nov 15, 2012</p>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -54,7 +54,7 @@ print_header('upcoming_events');
 			</div>
 			<div class="row">
 				<div class="twelve columns">
-          <h4>Event 5</h4>
+          <h4>Event 4</h4>
 					<div class="panel">
             <p>December 31, 2012</p>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -63,7 +63,7 @@ print_header('upcoming_events');
 			</div>
 			<div class="row">
 				<div class="twelve columns">
-          <h4>Event 6</h4>
+          <h4>Event 5</h4>
 					<div class="panel">
             <p>January 1, 2012</p>
 						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
