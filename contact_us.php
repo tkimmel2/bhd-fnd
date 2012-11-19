f4ef299d150b54f106ef7197ee29fa7aebfefcf2
Travis Kimmel <travis@brickhousedevelopment.com>
2012-11-18 18:23:06 -0700

    minor edits

--- contact_us.php
+++ contact_us.php
@@ -23,7 +23,7 @@ print_header('contact_us');
 				<p> If you're trying to contact us, you can reach us at (555) 555-5555 during normal business hours.  If you'd like to have us contact you, please fill out the form below and a representative will call or email you.
 					<!-- Row Layout for forms -->
 					<form>
-					  <label>Contact Information:</label>
+					  <label>Your Information:</label>
 					  <input type="text" placeholder="Name" />
 					  <div class="row">
 					    <div class="six columns">
